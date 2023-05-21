69e014983729888b830891f08a561b3e30295246
faisalsayed10 <faisal.sayed502@gmail.com>
2023-05-21 18:34:18 +0530

    feat: Hide sidebar for user dashboard

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,14 @@
+import { Dialog, Transition } from "@headlessui/react";
+import {
+  ArrowLeftOnRectangleIcon,
+  Bars3Icon,
+  CalendarIcon,
+  ChartPieIcon,
+  FolderIcon,
+  HomeIcon,
+  UsersIcon,
+  XMarkIcon
+} from "@heroicons/react/24/outline";
 import {
   User as SupabaseUser,
   createServerSupabaseClient,
@@ -5,37 +16,20 @@ import {
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { GetServerSidePropsContext } from "next";
 import { useRouter } from "next/router";
-import { useEffect, useState, Fragment } from "react";
-import AdminDashboard from "../components/admin/AdminDashboard";
-import UserDashboard from "../components/UserDashboard";
-import ChooseRole from "../components/ChooseRole";
+import { Fragment, useEffect, useState } from "react";
 import { toast } from "react-hot-toast";
-import { Dialog, Transition } from "@headlessui/react";
-import {
-  Bars3Icon,
-  CalendarIcon,
-  ChartPieIcon,
-  DocumentDuplicateIcon,
-  FolderIcon,
-  HomeIcon,
-  UsersIcon,
-  XMarkIcon,
-} from "@heroicons/react/24/outline";
-import { User } from "../lib/types";
 import { ThreeDots } from "react-loader-spinner";
+import ChooseRole from "../components/ChooseRole";
+import UserDashboard from "../components/UserDashboard";
+import AdminDashboard from "../components/admin/AdminDashboard";
+import { User } from "../lib/types";
 
 const navigation = [
-  { name: "Dashboard", href: "#", icon: HomeIcon, current: true },
-  { name: "Team", href: "#", icon: UsersIcon, current: false },
-  { name: "Projects", href: "#", icon: FolderIcon, current: false },
-  { name: "Calendar", href: "#", icon: CalendarIcon, current: false },
-  { name: "Documents", href: "#", icon: DocumentDuplicateIcon, current: false },
-  { name: "Reports", href: "#", icon: ChartPieIcon, current: false },
-];
-const teams = [
-  { id: 1, name: "Heroicons", href: "#", initial: "H", current: false },
-  { id: 2, name: "Tailwind Labs", href: "#", initial: "T", current: false },
-  { id: 3, name: "Workcation", href: "#", initial: "W", current: false },
+  { name: "Dashboard", href: "/", icon: HomeIcon, current: true },
+  { name: "Users", href: "/users", icon: UsersIcon, current: false },
+  { name: "Groups", href: "/groups", icon: CalendarIcon, current: false },
+  { name: "Projects", href: "/projects", icon: FolderIcon, current: false },
+  { name: "Tasks", href: "/tasks", icon: ChartPieIcon, current: false },
 ];
 
 function classNames(...classes: any) {
@@ -44,10 +38,11 @@ function classNames(...classes: any) {
 
 export default function Home({ user }: { user: SupabaseUser }) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
-
-  const supabase = useSupabaseClient();
   const [loading, setLoading] = useState(true);
   const [info, setInfo] = useState<User | null>(null);
+
+  const supabase = useSupabaseClient();
+  const router = useRouter();
 
   useEffect(() => {
     (async () => {
@@ -180,38 +175,6 @@ export default function Home({ user }: { user: SupabaseUser }) {
                             ))}
                           </ul>
                         </li>
-                        <li>
-                          <div className="text-xs font-semibold leading-6 text-gray-400">
-                            Your teams
-                          </div>
-                          <ul role="list" className="-mx-2 mt-2 space-y-1">
-                            {teams.map((team) => (
-                              <li key={team.name}>
-                                <a
-                                  href={team.href}
-                                  className={classNames(
-                                    team.current
-                                      ? "bg-gray-50 text-indigo-600"
-                                      : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50",
-                                    "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-                                  )}
-                                >
-                                  <span
-                                    className={classNames(
-                                      team.current
-                                        ? "text-indigo-600 border-indigo-600"
-                                        : "text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600",
-                                      "flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white"
-                                    )}
-                                  >
-                                    {team.initial}
-                                  </span>
-                                  <span className="truncate">{team.name}</span>
-                                </a>
-                              </li>
-                            ))}
-                          </ul>
-                        </li>
                       </ul>
                     </nav>
                   </div>
@@ -222,7 +185,13 @@ export default function Home({ user }: { user: SupabaseUser }) {
         </Transition.Root>
 
         {/* Static sidebar for desktop */}
-        <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-64 lg:flex-col">
+        <div
+          className={`${
+            info?.role === "user"
+              ? "hidden"
+              : "hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-64 lg:flex-col"
+          }`}
+        >
           {/* Sidebar component, swap this element with another sidebar if you like */}
           <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
             <div className="flex h-16 shrink-0 items-center">
@@ -262,61 +231,34 @@ export default function Home({ user }: { user: SupabaseUser }) {
                     ))}
                   </ul>
                 </li>
-                <li>
-                  <div className="text-xs font-semibold leading-6 text-gray-400">
-                    Your teams
-                  </div>
-                  <ul role="list" className="-mx-2 mt-2 space-y-1">
-                    {teams.map((team) => (
-                      <li key={team.name}>
-                        <a
-                          href={team.href}
-                          className={classNames(
-                            team.current
-                              ? "bg-gray-50 text-indigo-600"
-                              : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50",
-                            "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-                          )}
-                        >
-                          <span
-                            className={classNames(
-                              team.current
-                                ? "text-indigo-600 border-indigo-600"
-                                : "text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600",
-                              "flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white"
-                            )}
-                          >
-                            {team.initial}
-                          </span>
-                          <span className="truncate">{team.name}</span>
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </li>
                 <li className="-mx-6 mt-auto">
-                  <a
-                    href="#"
-                    className="flex items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-50"
+                  <button
+                    type="button"
+                    onClick={() => {
+                      supabase.auth.signOut();
+                      setTimeout(() => router.push("/login"), 1000);
+                    }}
+                    className="flex items-center gap-x-4 w-full px-6 py-3 text-sm font-semibold leading-6 text-gray-700 hover:text-gray-900"
                   >
-                    <img
-                      className="h-8 w-8 rounded-full bg-gray-50"
-                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                      alt=""
-                    />
-                    <span className="sr-only">Your profile</span>
-                    <span aria-hidden="true">Tom Cook</span>
-                  </a>
+                    <ArrowLeftOnRectangleIcon className="h-6 w-6 text-gray-400" />
+                    <span aria-hidden="true">Log Out</span>
+                  </button>
                 </li>
               </ul>
             </nav>
           </div>
         </div>
 
-        <div className="sticky top-0 z-40 flex items-center gap-x-6 bg-white px-4 py-4 shadow-sm sm:px-6 lg:hidden">
+        <div
+          className={`sticky top-0 z-40 flex items-center gap-x-6 bg-white px-4 py-4 shadow-sm sm:px-6 ${
+            info?.role === "user" ? "" : "lg:hidden"
+          }`}
+        >
           <button
             type="button"
-            className="-m-2.5 p-2.5 text-gray-700 lg:hidden"
+            className={`-m-2.5 p-2.5 text-gray-700 ${
+              info?.role === "user" ? "hidden" : "lg:hidden"
+            }`}
             onClick={() => setSidebarOpen(true)}
           >
             <span className="sr-only">Open sidebar</span>
@@ -325,24 +267,23 @@ export default function Home({ user }: { user: SupabaseUser }) {
           <div className="flex-1 text-sm font-semibold leading-6 text-gray-900">
             Dashboard
           </div>
-          <a href="#">
-            <span className="sr-only">Your profile</span>
-            <img
-              className="h-8 w-8 rounded-full bg-gray-50"
-              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-              alt=""
-            />
-          </a>
+          <button
+            type="button"
+            onClick={() => {
+              supabase.auth.signOut();
+              setTimeout(() => router.push("/login"));
+            }}
+          >
+            <ArrowLeftOnRectangleIcon className="h-6 w-6 text-gray-900" />
+          </button>
         </div>
 
-        <main className="py-10 lg:pl-64">
-          <div className="">
-            {info?.role === "admin" ? (
-              <AdminDashboard />
-            ) : info?.role === "user" ? (
-              <UserDashboard />
-            ) : null}
-          </div>
+        <main className={`py-10 ${info?.role === "user" ? "" : "lg:pl-64"}`}>
+          {info?.role === "admin" ? (
+            <AdminDashboard />
+          ) : info?.role === "user" ? (
+            <UserDashboard />
+          ) : null}
         </main>
       </div>
     </>
